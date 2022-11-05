8fcb4f6ae2d22eb453cdf342cae912a346a37f2a
franchi <Dyslate@gmail.com>
2022-11-05 16:55:45 +0100

    ajout d'un menu côté client, interception du menu côté serveur

--- client3.py
+++ client3.py
@@ -1,15 +1,48 @@
 from socket import *
 import sys
+from utils import *
+
 if len(sys.argv) != 3:
     sys.exit(-1)
 
+
+
+def menu(socket):
+    rep=True
+    while rep:
+        print("""
+        1.Jouer au pendu 1 Joueur
+        2.Jouer au pendu 2 joueurs
+        3.Chat
+        4.Exit/Quit
+        """)
+        rep=input("Que voulez-vous faire? ")
+        if rep=="1":
+            print("\nPendu 1J")
+            send(socket,"CODE001")
+            rep = None
+        elif rep=="2":
+            print("\n Pendu 2J")
+            send(socket,"CODE002")
+            rep = None
+        elif rep=="3":
+            print("\n Chat")
+            send(socket,"CODE003")
+            rep = None
+        elif rep=="4":
+            print("\n Quitter") 
+            rep = None
+            socket.close()
+            break
+        else:
+            print("\n Veuillez selectionner un choix valide")
 #Connection socket
 host = sys.argv[1]
 port = int(sys.argv[2])
 s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
 s.connect((host, port))
 
-
+menu(s)
 while True:
     message_recu = s.recv(1000)
     message_recu = str(message_recu,'utf-8')
